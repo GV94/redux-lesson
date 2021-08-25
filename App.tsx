8777a97afd5227fec5356b87e49aebed452d5d62
Victor <victorgustafsson@hotmail.com>
2021-08-25 10:37:29 +0200

    make app a clean .tsx file

--- App.tsx
+++ App.tsx
@@ -1,32 +1,5 @@
 import React, { FC } from "react";
 
-/* Walkthrough */
-/* 1st: What are hooks? */
-/* 2nd: Native hooks (useState, useEffect, useLayoutEffect, useContext, useMemo, ) */
-/* 3rd: Custom hooks */
-
-/* Try it yourself */
-/* 4th: Create a useButton hook that returns a hook that updates the state of the component that uses it */
-/* 5th: BONUS: Create a hook that returns if the screen is currently mobile-, tablet- or desktop-sized */
-
-const Header = () => {
-  return <h1>Hooks Lesson! 🎣</h1>;
-};
-
-const Body = () => {
-  return (
-    <div>
-      {/* Your code here */}
-      {/* Your code here */}
-    </div>
-  );
-};
-
 export const App: FC = () => {
-  return (
-    <div>
-      <Header />
-      <Body />
-    </div>
-  );
+  return <div>React Boilerplate</div>;
 };
